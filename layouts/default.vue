28a39209b3a8da2c1637db9de284520e4c5ff40b
oscarf4 <oscarvedo97@gmail.com>
2022-04-05 00:38:38 -0500

    Primer commit del template de la Cake Store: Navbar y parte de organización

---
+++ layouts/default.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-app>
+<!--    <header class="header">-->
+<!--      <p class="header__text">Envíos gratis desde $55.000</p>-->
+<!--    </header>-->
+    <nav class="navbar">
+      <div class="nav__wrapper">
+        <v-icon
+          size="29"
+          class="nav__wrapper__icon"
+          color="rgb(90,90,90)"
+        >mdi-menu
+        </v-icon>
+      </div>
+      <div class="navbar__logo">
+        <img class="navbar__logo__image"
+             src="https://firebasestorage.googleapis.com/v0/b/real-web-project.appspot.com/o/Cake%20shop%20(1).png?alt=media&token=40ccd33d-4c1f-47d0-a2f7-ab36706bc9b8"
+             alt="">
+      </div>
+      <div class="navbar__actions">
+        <v-icon size="29" color="rgb(90,90,90)" class="navbar__actions__icon">mdi-account</v-icon>
+        <v-icon size="29" color="rgb(90,90,90)" class="navbar__actions__icon">mdi-magnify</v-icon>
+        <v-icon size="29" color="rgb(90,90,90)" class="navbar__actions__icon">mdi-cart</v-icon>
+      </div>
+    </nav>
+    <nuxt/>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  height: 35px;
+  background-color: sandybrown;
+}
+.header__text {
+  margin: auto;
+  font-size: 15px;
+  font-weight: bold;
+}
+.navbar {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  list-style: none;
+  padding: 0;
+  height: 72px;
+  margin: 15px 0;
+  padding: 0 30px
+}
+.nav__wrapper {
+  display: flex;
+  align-items: center;
+}
+.nav__wrapper__icon {
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+}
+.navbar__logo {
+  display: flex;
+}
+.navbar__logo__image {
+  height: 72px;
+  width: auto;
+  margin: auto;
+}
+.navbar__actions {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+}
+.navbar__actions__icon {
+  margin-left: 14px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <section class="main-banner">
+    </section>
+
+    <section class="">
+      <h1 class="text-center my-8">Shop By Category</h1>
+
+      <v-row style="height: 310px;width: 100%;margin: 0;">
+        <v-col class="first__category ml-4 mr-2">
+
+        </v-col>
+        <v-col class="first__category ml-2 mr-4">
+
+        </v-col>
+      </v-row>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Cakeland - Tortas de matrimonio, compromiso, cumpleaños y para todos tus eventos',
+      meta: [{
+        name: 'description',
+        content: 'Mercado sin salir de casa, mercado a domicilio en Barrancabermeja.',
+        hid: 'description'
+      }]
+    }
+  },
+}
+</script>
+
+<style scoped>
+.main-banner {
+  height: 690px;
+  background-image: url("https://cdn.shopify.com/s/files/1/0509/8615/6220/files/Cubicake_line_up_1944x.jpg?v=1611934222");
+  background-size: cover;
+  background-position: center;
+}
+@media (max-width: 1400px) and (min-width: 1000px) {
+  .main-banner {
+    height: 500px;
+  }
+}
+@media (max-width: 999px) and (min-width: 0) {
+  .main-banner {
+    height: 350px;
+  }
+}
+.first__category {
+  background-image: url("https://cdn.shopify.com/s/files/1/0509/8615/6220/collections/Evoke_20Pictures_20Lifestyle_Creative_20Shoot_80_900x.jpg?v=1611936089");
+  background-size: auto;
+  background-position: center;
+}
+</style>
